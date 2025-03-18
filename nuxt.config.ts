a00ef4a2dd7c346e0632e91be1e7d3e6b247266e
Andre Monteiro <andrefmonteiro@outlook.com>
2025-03-18 20:52:09 +0000

    supabase env data added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  runtimeConfig: {
+    supabaseDbPassowrd: process.env.SUPABASE_DB_PASSWORD,
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY
+    }
+  }
 })
