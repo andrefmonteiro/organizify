50e10078e6de6079715d1f74fda8586c5fcfc3e0
Andre Monteiro <andrefmonteiro@outlook.com>
2025-03-21 22:06:29 +0000

    shadcn-nuxt module added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   runtimeConfig: {
     supabaseDbPassowrd: process.env.SUPABASE_DB_PASSWORD,
     public: {
