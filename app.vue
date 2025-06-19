e41dbd08ceefac53a7f5bad9a9b5acf3b7a46f22
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-19 18:15:54 +0100

    removed navmenu, removed prettier as a dev dependency

--- app.vue
+++ app.vue
@@ -1,29 +1,15 @@
 <script setup lang="ts">
-import NavigationMenu from './components/common/navigation-menu/NavigationMenu.vue';
-import NavigationMenuContent from './components/common/navigation-menu/NavigationMenuContent.vue';
-import NavigationMenuItem from './components/common/navigation-menu/NavigationMenuItem.vue';
-import NavigationMenuLink from './components/common/navigation-menu/NavigationMenuLink.vue';
-import NavigationMenuList from './components/common/navigation-menu/NavigationMenuList.vue';
-import NavigationMenuTrigger from './components/common/navigation-menu/NavigationMenuTrigger.vue';
-import Switch from './components/ui/switch/Switch.vue';
-
-
+import Switch from './components/ui/switch/Switch.vue'
 </script>
 
 <template>
   <div>
-    <p class="text-green-800">Hello World</p>
-    <Button size="lg">Click me</Button>
-    <Switch/>
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <p class="text-green-800">
+      Hello World
+    </p>
+    <Button size="lg">
+      Click me
+    </Button>
+    <Switch />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxtjs/tailwindcss',
-    'shadcn-nuxt'
+    'shadcn-nuxt',
   ],
+  devtools: { enabled: true },
+  runtimeConfig: {
+    supabaseDbPassowrd: process.env.SUPABASE_DB_PASSWORD,
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -17,18 +29,6 @@ export default defineNuxtConfig({
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: './components/ui',
   },
-  eslint: {
-    config: {
-      stylistic: true
-    }
-  },
-  runtimeConfig: {
-    supabaseDbPassowrd: process.env.SUPABASE_DB_PASSWORD,
-    public: {
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseKey: process.env.SUPABASE_KEY
-    }
-  }
 })
