9a131720b6eff364aaba2545a673ab5ad8981471
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-20 01:05:17 +0100

    changed placeholder

--- app.vue
+++ app.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import Switch from './components/ui/switch/Switch.vue'
 </script>
 
 <template>
   <div>
-    <p class="text-green-800">
+    <p class="text-green-800 uppercase">
       Hello World
     </p>
-    <Button size="lg">
-      Click me
-    </Button>
-    <Switch />
   </div>
 </template>
