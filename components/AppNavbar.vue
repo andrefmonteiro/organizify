0ae82f938a3027e9e7ad69e36a8a2161cd395e2d
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-21 23:07:14 +0100

    logo now links to homepage

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -2,9 +2,13 @@
   <header class="absolute inset-x-0 top-0 z-50 bg-muted">
     <nav class="flex items-center justify-between px-6 py-4 lg:px-8">
       <div class="flex items-center">
-        <h1 class="text-2xl font-bold">
-          Organizify
-        </h1>
+        <NuxtLink
+          to="/"
+        >
+          <h1 class="text-2xl font-bold hover:opacity-80 transition-opacity">
+            Organizify
+          </h1>
+        </NuxtLink>
       </div>
 
       <div class="flex items-center">
