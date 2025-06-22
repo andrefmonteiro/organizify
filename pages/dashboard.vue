d058089e168674a0705e11389312fa26ed958c79
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-22 17:22:23 +0100

    dashboard title added

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,8 @@
 <template>
 	<div>
-		<p>Dashboard page</p>
+		<h2 class="text-3xl font-bold tracking-tight sm:text-4xl pt-16 pb-6">
+			Dashboard
+		</h2> <!-- dunno if h2 is the most semanctically appropriate -->
+		<Separator />
 	</div>
 </template>
