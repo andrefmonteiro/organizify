9d45ff88cbe1bc6d897525dd83a7ae183746b727
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-22 16:39:35 +0100

    footer is now sticky to the bottom of the page; footer resized

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="min-h-screen bg-background text-foreground lg:grid lg:grid-rows-[1fr_auto] lg:grid-cols-[1fr_minmax(0,6fr)_1fr] lg:gap-x-6 lg:px-8">
+  <div class="min-h-screen bg-background text-foreground grid grid-rows-[1fr_auto] lg:grid-cols-[1fr_minmax(0,6fr)_1fr] lg:gap-x-6 lg:px-8">
     <AppNavbar />
 
     <main class="px-6 pt-16 lg:col-start-2 lg:px-0">
       <slot />
     </main>
 
-    <AppFooter />
+    <div class="lg:col-span-3">
+      <AppFooter />
+    </div>
   </div>
 </template>
