3281d32d8e2d0627bb1381ed43684a6efb5b5b8c
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-22 20:17:36 +0100

    removed comment and import

--- components/PlaylistSyncSettings.vue
+++ components/PlaylistSyncSettings.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { FeatureToggleCard } from '#components'
 </script>
 
 <template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h1 class="text-3xl font-bold tracking-tight sm:text-4xl pt-16 pb-6">
 			Dashboard
-		</h1> <!-- dunno if h2 is the most semanctically appropriate -->
+		</h1>
 		<Separator />
 		<WeeklyPlaylistsToggle />
 	</div>
