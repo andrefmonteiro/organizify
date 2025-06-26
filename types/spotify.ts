8ccd9aa198eae146a1a7ead5800a448ca4b435f5
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-26 23:13:35 +0100

    spotify profile metadata type defintion changed for the flatter object that is retrieved from OAuth

--- types/spotify.ts
+++ types/spotify.ts
@@ -1,26 +1,9 @@
-export interface SpotifyUser {
-	country: string
-	display_name: string
-	email: string
-	explicit_content: {
-		filter_enabled: false
-		filter_locked: false
-	}
-	external_urls: {
-		spotify: string
-	}
-	followers: {
-		href: string
-		total: number
-	}
-	href: string
-	id: string
-	images: Array<{
-		url: string
-		height: number
-		width: number
-	}>
-	product: string
-	type: string
-	uri: string
+export interface SpotifyProfileMetadata {
+	full_name?: string
+	avatar_url: string
+	email?: string
+	email_verified?: boolean
+	sub?: string
+	id?: string
+	iss?: string
 }
