c7e669c5cf6ad2d883d341e91f4abc0262cb663d
Andre Monteiro <andrefmonteiro@outlook.com>
2025-06-28 03:21:57 +0100

    conditionally rendering alert when user is logged in but doesn't have spotify permissions anymore

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,53 @@
 <script setup>
+import { TriangleAlert } from 'lucide-vue-next'
+import Alert from '~/components/ui/alert/Alert.vue'
+import AlertDescription from '~/components/ui/alert/AlertDescription.vue'
+import AlertTitle from '~/components/ui/alert/AlertTitle.vue'
+
+definePageMeta({
+	middleware: 'spotify-permissions',
+})
+// const { permissionStatus } = useSpotifyPermissions()
+const { signInWithSpotify } = useAuthActions()
+const { permissionStatus } = useSpotifyPermissions()
 const { displayName } = useSpotifyProfile()
+
+const handleReconnect = async () => {
+	await signInWithSpotify()
+}
 </script>
 
 <template>
 	<div>
+		<Alert
+			v-if="permissionStatus === 'invalid'"
+			class="mt-4"
+			variant="warning"
+		>
+			<TriangleAlert class="w-4 h-4" />
+			<AlertTitle>You're not connected</AlertTitle>
+			<AlertDescription class="mt-2">
+				Your Spotify permissions have been revoked or expired.
+				Please reconnect your account to use Organizify's features.
+			</AlertDescription>
+			<div class="flex justify-start pl-6">
+				<Button
+					size=""
+					class="mt-4 cursor-pointer"
+					@click="handleReconnect"
+				>
+					Reconnect Spotify
+				</Button>
+			</div>
+		</Alert>
 		<h1 class="pt-16 pb-6 text-3xl font-bold tracking-tight sm:text-4xl">
 			Hello{{ displayName ? `, ${displayName}` : '' }}!
 		</h1>
 		<Separator />
-		<WeeklyPlaylistsToggle />
+		<div
+			:class="{ 'opacity-50 pointer-events-none': permissionStatus === 'invalid' }"
+		>
+			<WeeklyPlaylistsToggle />
+		</div>
 	</div>
 </template>
