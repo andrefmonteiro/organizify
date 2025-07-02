c99e36ef5bd6532171d3a78b2f566a354f7352af
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-02 22:36:33 +0100

    basic UI refinement

--- components/LikedSongsOrganizationSettings.vue
+++ components/LikedSongsOrganizationSettings.vue
@@ -4,9 +4,12 @@
 <template>
 	<div class="w-full max-w-lg space-y-6">
 		<div>
+			<h2 class="mt-12 mb-4 text-lg font-medium">
+				Liked Songs
+			</h2>
 			<div class="space-y-4">
 				<FeatureToggleCard
-					title="Organize Liked Songs"
+					title="Organize by genre"
 					description="Creates genre-themed playlists of your Liked Songs"
 				/>
 			</div>
