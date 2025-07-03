b0d2836d41b6bc8df0c24daf3ea3320ddb774a1b
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-03 02:50:28 +0100

    genre mapping refactored: less overlap and improved categorizations to be more accurate

--- composables/useGenreOrganization.ts
+++ composables/useGenreOrganization.ts
@@ -13,31 +13,27 @@ export const useGenreOrganization = () => {
 	 */
 	const GENRE_MAPPING = {
 		Pop: [
-			'pop', 'dance pop', 'pov: indie', 'latin pop', 'new wave pop', 'indonesian pop',
-			'k-pop', 'singer-songwriter pop', 'modern country pop', 'alt z', 'opm',
-			'indietronica', 'turkish pop', 'italian pop', 'j-pop', 'mexican pop',
-			'indie pop', 'spanish pop', 'sertanejo pop', 'latin arena pop', 'synthpop',
-			'sped up', 'hip pop', 'desi pop', 'eurodance', 'italian adult pop', 'ccm',
-			'french pop', 'german pop', 'post-teen pop', 'art pop', 'nigerian pop',
-			't-pop', 'gym phonk', 'europop', 'chamber pop', 'mandopop', 'uk pop',
-			'punjabi pop', 'anime', 'adoracao', 'swedish pop', 'v-pop', 'neon pop punk',
-			'christian music', 'worship', 'bedroom pop', 'classic italian pop',
+			'pop', 'dance pop', 'latin pop', 'new wave pop', 'indonesian pop', 'k-pop',
+			'singer-songwriter pop', 'modern country pop', 'alt z', 'opm', 'indietronica',
+			'turkish pop', 'italian pop', 'j-pop', 'mexican pop', 'indie pop', 'spanish pop',
+			'sertanejo pop', 'latin arena pop', 'synthpop', 'sped up', 'hip pop', 'desi pop',
+			'eurodance', 'italian adult pop', 'french pop', 'german pop', 'post-teen pop',
+			'art pop', 'nigerian pop', 't-pop', 'europop', 'chamber pop', 'mandopop', 'uk pop',
+			'punjabi pop', 'swedish pop', 'v-pop', 'neon pop punk', 'classic italian pop',
 			'k-pop girl group', 'britpop', 'polish pop', 'chinese viral pop', 'danish pop',
-			'dutch pop', 'pixel', 'otacore', 'arab pop', 'girl group', 'meme',
-			'gen z singer-songwriter', 'afropop', 'k-pop boy group', 'folk-pop',
-			'russian pop', 'electropop', 'classic country pop', 'trip hop', 'cumbia pop',
-			'talent show', 'french indie pop', 'latin viral pop', 'boy band',
-			'reggae fusion', 'swedish trap pop', '5th gen k-pop', 'dutch rap pop',
-			'colombian pop', 'korean pop', 'acoustic pop', 'indie poptimism',
-			'country pop', 'malaysian pop', 'neo-synthpop', 'arabesk', 'bubblegum pop',
-			'cantopop', 'thai pop', 'vocaloid', 'dream pop', 'finnish pop',
-			'canadian pop', 'viral pop', 'classic swedish pop', 'funk rj',
+			'dutch pop', 'arab pop', 'girl group', 'meme', 'gen z singer-songwriter',
+			'afropop', 'k-pop boy group', 'folk-pop', 'russian pop', 'electropop',
+			'classic country pop', 'cumbia pop', 'talent show', 'french indie pop',
+			'latin viral pop', 'boy band', 'reggae fusion', '5th gen k-pop', 'colombian pop',
+			'korean pop', 'acoustic pop', 'country pop', 'malaysian pop', 'neo-synthpop',
+			'arabesk', 'bubblegum pop', 'cantopop', 'thai pop', 'finnish pop', 'canadian pop',
+			'viral pop', 'classic swedish pop', 'funk rj',
 		],
 		Rock: [
 			'rock', 'alternative rock', 'indie rock', 'classic rock', 'punk', 'punk rock',
 			'garage rock', 'psychedelic rock', 'grunge', 'alternative', 'indie', 'emo',
 			'post-punk', 'new wave', 'britrock', 'college rock', 'modern rock', 'art rock',
-			'progressive rock', 'glam rock', 'southern rock', 'blues rock', 'folk rock',
+			'progressive rock', 'glam rock', 'southern rock', 'blues rock',
 			'country rock', 'roots rock', 'heartland rock', 'arena rock', 'surf rock',
 			'garage rock revival', 'post-punk revival', 'neo-psychedelic',
 		],
@@ -54,26 +50,27 @@ export const useGenreOrganization = () => {
 			'melodic hardcore', 'spanish metal', 'trancecore', 'post-metal',
 		],
 		Hip_Hop: [
-			'hip hop', 'rap', 'trap', 'drill', 'grime', 'conscious hip hop', 'east coast hip hop',
+			'hip hop', 'rap', 'drill', 'grime', 'conscious hip hop', 'east coast hip hop',
 			'west coast rap', 'southern hip hop', 'gangsta rap', 'alternative hip hop',
 			'experimental hip hop', 'old school hip hop', 'boom bap', 'cloud rap',
 			'mumble rap', 'latin hip hop', 'uk hip hop', 'french hip hop', 'german hip hop',
+			'trap', 'gym phonk', 'swedish trap pop', 'dutch rap pop',
 		],
 		Electronic: [
 			'electronic', 'house', 'techno', 'dubstep', 'edm', 'ambient', 'trance',
 			'drum and bass', 'breakbeat', 'garage', 'jungle', 'hardcore', 'hardstyle',
 			'progressive house', 'deep house', 'tech house', 'minimal techno', 'acid house',
-			'synthwave', 'vaporwave', 'chillwave', 'downtempo', 'trip hop', 'idm',
-			'electronica', 'electro', 'disco', 'nu-disco', 'future bass', 'trap',
-			'glitch', 'drone', 'dark ambient', 'new age',
+			'synthwave', 'vaporwave', 'chillwave', 'downtempo', 'idm', 'electronica',
+			'electro', 'disco', 'nu-disco', 'future bass', 'glitch', 'drone', 'dark ambient',
+			'new age',
 		],
 		R_and_B: [
 			'r&b', 'soul', 'funk', 'neo soul', 'contemporary r&b', 'quiet storm',
-			'new jack swing', 'hip hop soul', 'alternative r&b', 'gospel', 'urban contemporary',
+			'new jack swing', 'hip hop soul', 'alternative r&b', 'urban contemporary',
 		],
 		Country: [
-			'country', 'bluegrass', 'americana', 'alt-country', 'country rock', 'outlaw country',
-			'contemporary country', 'country pop', 'honky tonk', 'western', 'folk country',
+			'country', 'bluegrass', 'alt-country', 'outlaw country',
+			'honky tonk', 'western', 'contemporary country',
 		],
 		Jazz: [
 			'jazz', 'bebop', 'smooth jazz', 'fusion', 'cool jazz', 'hard bop', 'free jazz',
@@ -90,14 +87,24 @@ export const useGenreOrganization = () => {
 		],
 		Folk_Acoustic: [
 			'folk', 'acoustic', 'singer-songwriter', 'indie folk', 'alternative country',
-			'americana', 'traditional folk', 'contemporary folk', 'folk rock', 'acoustic rock',
+			'americana', 'traditional folk', 'contemporary folk', 'acoustic rock',
 			'fingerstyle', 'celtic', 'world music', 'protest song', 'roots', 'banjo',
 			'mandolin', 'bluegrass', 'country folk', 'irish folk', 'scottish folk',
+			'bedroom pop', 'indie poptimism', 'dream pop', 'folk rock',
 		],
 		Latin: [
 			'latin', 'salsa', 'reggaeton', 'bachata', 'merengue', 'cumbia', 'bossa nova',
-			'samba', 'tango', 'flamenco', 'mariachi', 'tejano', 'latin rock', 'latin pop',
+			'samba', 'tango', 'flamenco', 'mariachi', 'tejano', 'latin rock',
 			'nueva cancion', 'bolero', 'mambo', 'cha cha cha', 'rumba',
+		],
+		Anime_Japanese: [
+			'anime', 'otacore', 'pixel', 'vocaloid',
+		],
+		Gospel_Worship: [
+			'christian music', 'worship', 'ccm', 'adoracao', 'gospel',
+		],
+		Downtempo: [
+			'trip hop',
 		],
 	}
 
@@ -106,9 +113,7 @@ export const useGenreOrganization = () => {
 	 */
 	const mapGenresToBroadCategories = (spotifyGenres: string[]): string => {
 		// Check each genre in priority order (first genre is most important)
-		for (let i = 0; i < spotifyGenres.length; i++) {
-			const genreToCheck = spotifyGenres[i]
-
+		for (const genreToCheck of spotifyGenres) {
 			// See if this specific genre matches any of our broad categories
 			for (const [broadGenre, subgenres] of Object.entries(GENRE_MAPPING)) {
 				const hasMatch = subgenres.some(mappedGenre =>
