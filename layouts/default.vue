77c11e1c11e9e491d3a8c158ae8c43d8483ae116
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-11 17:54:22 +0100

    minor style changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="min-h-screen bg-background text-foreground grid grid-rows-[1fr_auto] lg:grid-cols-[1fr_minmax(0,6fr)_1fr] lg:gap-x-6 lg:px-8">
+	<div class="min-h-screen bg-surface-default text-foreground grid grid-rows-[1fr_auto] lg:grid-cols-[1fr_minmax(0,6fr)_1fr] lg:gap-x-6 lg:px-8">
 		<AppNavbar />
 
 		<main class="px-6 pt-16 lg:col-start-2 lg:px-0">
