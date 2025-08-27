9804d39ca46d19b574113c4f6f03a69ce2a90b0a
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-27 21:43:28 +0100

    perfecting the pixels

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="min-h-screen bg-surface-default text-foreground grid grid-rows-[1fr_auto] lg:grid-cols-[1fr_minmax(0,6fr)_1fr] lg:gap-x-6 lg:px-8">
+	<div class=" bg-surface-default text-foreground grid grid-rows-[1fr_auto] lg:grid-cols-[1fr_minmax(0,6fr)_1fr] lg:gap-x-6 lg:px-8">
 		<AppNavbar />
 
-		<main class="px-6 pt-16 lg:col-start-2 lg:px-0">
+		<main class="px-6 lg:col-start-2 lg:px-0">
 			<slot />
 		</main>
 
