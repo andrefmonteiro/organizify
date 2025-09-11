d7559777c457dab20fd915a23f7e44de4b6349e8
Andre Monteiro <andrefmonteiro@outlook.com>
2025-09-11 19:25:54 +0100

    shameless plug

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,7 +6,13 @@ const currYear = new Date().getFullYear()
 	<footer class="mt-16 bg-muted/5 lg:col-span-3">
 		<div class="px-6 py-12 mx-auto max-w-7xl lg:px-8">
 			<div class="text-sm text-center text-muted-foreground">
-				<p>&copy; {{ currYear }} Organizify</p>
+				<p>
+					&copy; {{ currYear }} Organizify. Made by <a
+						class="underline"
+						href="https://andremonteiro.me"
+						target="_blank"
+					>André Monteiro</a>
+				</p>
 			</div>
 		</div>
 	</footer>
